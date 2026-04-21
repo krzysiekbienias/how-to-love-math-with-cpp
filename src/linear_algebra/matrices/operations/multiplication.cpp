b8c2e7f2605d1e96a8308cbac340a786e4e3d2ac
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-04-21 21:08:35 +0200

    chore(core_lib): update CMakeLists with new sources and table of contents in README

--- src/linear_algebra/matrices/operations/multiplication.cpp
+++ src/linear_algebra/matrices/operations/multiplication.cpp
@@ -1,33 +1,61 @@
 #include <vector>
 #include <linear_algebra/matrices/operations/multiplication.hpp>
 
-std::vector<std::vector<int>> multiply(const std::vector<std::vector<int>>&A,const std::vector<std::vector<int>>&B) {
-    int mA=0;
-    int mB=0;
-    int nA=0;
-    int nB=0;
-    if (!A.empty()&& !B.empty()) {
-        mA= A.size();
-        mB= B.size();
-        nA= A[0].size();
-        nB= B[0].size();
-
+std::vector<std::vector<int> > multiply(const std::vector<std::vector<int> > &A,
+                                        const std::vector<std::vector<int> > &B) {
+    int mA = 0;
+    int mB = 0;
+    int nA = 0;
+    int nB = 0;
+    if (!A.empty() && !B.empty()) {
+        mA = A.size();
+        mB = B.size();
+        nA = A[0].size();
+        nB = B[0].size();
     }
-    if (nA!=mB) {
-        throw std::runtime_error("Not possible to ");
+    if (nA != mB) {
+        throw std::runtime_error("Incompatible matrix dimensions");
     }
-    std::vector<std::vector<int>> C(mA,std::vector<int> (nB,0));
+    std::vector<std::vector<int> > C(mA, std::vector<int>(nB, 0));
     for (int i = 0; i < mA; ++i) {
         for (int j = 0; j < nB; ++j) {
             for (int k = 0; k < nA; ++k) {
-                C[i][j]+=A[i][k]*B[k][j];
+                C[i][j] += A[i][k] * B[k][j];
             }
-
         }
-
     }
 
-
     return C;
-
 }
+
+
+std::vector<std::vector<int> > sparseMatrixMultiplication(const std::vector<std::vector<int> > &A,
+                                                          const std::vector<std::vector<int> > &B) {
+    int mA = 0;
+    int mB = 0;
+    int nA = 0;
+    int nB = 0;
+    if (!A.empty() && !B.empty()) {
+        mA = A.size();
+        mB = B.size();
+        nA = A[0].size();
+        nB = B[0].size();
+    }
+    if (nA != mB) {
+        throw std::runtime_error("Incompatible matrix dimensions");
+    }
+    std::vector<std::vector<int> > C(mA, std::vector<int>(nB, 0));
+    for (int i = 0; i < mA; ++i) {
+        for (int k = 0; k < nA; ++k) {
+            if (A[i][k] == 0) continue;
+            for (int j = 0; j < nB; ++j) {
+                if (B[k][j] == 0) continue;
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return C;
+}
+
+
+
